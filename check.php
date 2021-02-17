97360185aff658cb1eca45585f882b887b313bd4
Hanna <35894454+Annmeyer-bs@users.noreply.github.com>
2021-02-17 17:04:20 +0200

    Update check.php

--- check.php
+++ check.php
@@ -12,8 +12,3 @@ mysqli_query($connect,"INSERT INTO `user`( `full_name`,`email`,`login`, `passwor
 <<<<<<< HEAD
 $_SESSION['Message']='Регистрация	прошла	успешна';
 header('Location:	/check_in2/index.php');
-	?>
-=======
-	header('Location:	/check_in/index.php');
-	?>
->>>>>>> 1854e9c1eb0cdc12a7fc7399489fb57ed16b5f0f
