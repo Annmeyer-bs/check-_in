0bb95c02ab324797852086bf7650a291bf10d6ab
Hanna <35894454+Annmeyer-bs@users.noreply.github.com>
2021-02-18 13:05:27 +0200

    Update lkup.php

--- lkup.php
+++ lkup.php
@@ -10,13 +10,13 @@ $phone=$_POST['phone'];
 $avat='ava/'	.	time()	.	$_FILES['ava']['name'];
 if(move_uploaded_file($_FILES['ava']['tmp_name'],	$avat)){
 	$_SESSION['Message']='Загрузка	фото	неудалась';
-	header('Location:	/check_in2/lk.php');	
+	header('Location:	lk.php');	
 }
 
 	$id=$_SESSION['user']['id'];
 $res=mysqli_query($connect,"UPDATE `user` SET	`full_name`='$full_name',`email`='$email',`phone`='$phone', `ava`='$avat'	WHERE	`id`='$id'");
 
 $_SESSION['Message']='Изменения	прошли	успешно';
-header('Location:	/check_in2/lk.php');
+header('Location:	lk.php');
 
 ?>
